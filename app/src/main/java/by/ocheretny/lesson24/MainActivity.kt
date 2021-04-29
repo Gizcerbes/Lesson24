5ab7bf062545a302bae75cb36155bcb1d4d043a0
Gizcerbes <gizcerbes@outlook.com>
2021-04-29 20:33:45 +0300

    Create MainViewModel.kt

--- app/src/main/java/by/ocheretny/lesson24/MainActivity.kt
+++ app/src/main/java/by/ocheretny/lesson24/MainActivity.kt
@@ -5,15 +5,15 @@ import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
-    lateinit var viewModel: MainViewModel
+    val viewModel by lazy { ViewModelProvider(this).get(MainViewModel::class.java) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        ViewModelProvider(this).get
-
         supportFragmentManager.beginTransaction().replace(R.id.conteiner_main, FragmentShowRating())
+
+
 
     }
 }
