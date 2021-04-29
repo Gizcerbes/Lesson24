f61492afccefea9c9fea36fb1284273f9b0d5d46
Gizcerbes <gizcerbes@outlook.com>
2021-04-29 20:51:01 +0300

    Update FragmentUpdateCharacter.kt

--- app/src/main/java/by/ocheretny/lesson24/FragmentUpdateCharacter.kt
+++ app/src/main/java/by/ocheretny/lesson24/FragmentUpdateCharacter.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.textfield.TextInputLayout
@@ -22,11 +23,20 @@ class FragmentUpdateCharacter : Fragment() {
         val changePhoto = view.findViewById<TextInputLayout>(R.id.change_photo)
         val plusGames = view.findViewById<TextInputLayout>(R.id.plus_games)
         val plusWins = view.findViewById<TextInputLayout>(R.id.plus_wins)
-        val plusKills = view.findViewById<TextInputLayout>(R.id.plus_wins)
+        val plusKills = view.findViewById<TextInputLayout>(R.id.plus_kills)
 
         val character : Character?  = viewModel.selectedCharacter.value
 
+        changePhoto.editText?.setText(character?.photoURL)
+        plusGames.editText?.setText("0")
+        plusWins.editText?.setText("0")
+        plusKills.editText?.setText("0")
 
+        view.findViewById<Button>(R.id.character_update_button).setOnClickListener {
+            character?.photoURL = changePhoto.editText?.text.toString()
+            character?.countGames = character?.countGames?.plus(plusGames.editText?.text.toString().toInt())!!
+            character?.countKills = character?.countKills?.plus(plusKills.editText?.text.toString().toInt())!!
+            character?.countWins = character?.countWins?.plus(plusWins.editText?.text.toString().toInt())!!
+        }
     }
-
 }
